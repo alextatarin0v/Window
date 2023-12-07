115bc4ba8aabf018b386c0bfc4d7758d2b948e1f
Alex <starboyalxs@mail.ru>
2023-12-07 18:48:09 +0300

    220-221: working with a calculator form

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,22 +1,19 @@
-const forms = () => {
-	const form = document.querySelectorAll('form'),
-		inputs = document.querySelectorAll('input'),
-		phoneInputs = document.querySelectorAll('input[name="user_phone"]');
+import checkNumInputs from "./checkNumInputs";
 
-	phoneInputs.forEach(item => {
-		item.addEventListener('input', () => {
-			item.value = item.value.replace(/\D/, '');
-		});
-	});
+const forms = (state) => {
+	const form = document.querySelectorAll("form"),
+		inputs = document.querySelectorAll("input");
+
+	checkNumInputs("input[name=\"user_phone\"]");
 
 	const message = {
-		loading: 'Загрузка...',
-		success: 'Спасибо! Скоро мы с вами свяжемся',
-		failure: 'Что-то пошло не так...'
+		loading: "Загрузка...",
+		success: "Спасибо! Скоро мы с вами свяжемся",
+		failure: "Что-то пошло не так..."
 	};
 
 	const postData = async (url, data) => {
-		document.querySelector('.status').textContent = message.loading;
+		document.querySelector(".status").textContent = message.loading;
 		let res = await fetch(url, {
 			method: "POST",
 			body: data
@@ -27,21 +24,26 @@ const forms = () => {
 
 	const clearInputs = () => {
 		inputs.forEach(item => {
-			item.value = '';
+			item.value = "";
 		});
 	};
 
 	form.forEach(item => {
-		item.addEventListener('submit', (e) => {
+		item.addEventListener("submit", (e) => {
 			e.preventDefault();
 
-			let statusMessage = document.createElement('div');
-			statusMessage.classList.add('status');
+			let statusMessage = document.createElement("div");
+			statusMessage.classList.add("status");
 			item.appendChild(statusMessage);
 
 			const formData = new FormData(item);
+			if (item.getAttribute("data-calc") === "end") {
+				for (let key in state) {
+					formData.append(key, state[key]);
+				}
+			}
 
-			postData('assets/server.php', formData)
+			postData("assets/server.php", formData)
 				.then(res => {
 					console.log(res);
 					statusMessage.textContent = message.success;
--- src/js/slider.js
+++ src/js/slider.js
@@ -10,37 +10,37 @@ $(document).ready(function () {
 			breakpoint: 1201,
 			settings: {
 				slidesToShow: 4,
-				prevArrow: '<button class="prev arrow"></button>',
-				nextArrow: '<button class="next arrow"></button>',
+				prevArrow: "<button class=\"prev arrow\"></button>",
+				nextArrow: "<button class=\"next arrow\"></button>",
 				slidesToScroll: 1
 			}
 		}, {
 			breakpoint: 992,
 			settings: {
 				slidesToShow: 3,
-				prevArrow: '<button class="prev arrow"></button>',
-				nextArrow: '<button class="next arrow"></button>',
+				prevArrow: "<button class=\"prev arrow\"></button>",
+				nextArrow: "<button class=\"next arrow\"></button>",
 				slidesToScroll: 2
 			}
 		}, {
 			breakpoint: 768,
 			settings: {
 				slidesToShow: 2,
-				prevArrow: '<button class="prev arrow"></button>',
-				nextArrow: '<button class="next arrow"></button>',
+				prevArrow: "<button class=\"prev arrow\"></button>",
+				nextArrow: "<button class=\"next arrow\"></button>",
 				slidesToScroll: 2
 			}
 		}, {
 			breakpoint: 530,
 			settings: {
 				slidesToShow: 1,
-				prevArrow: '<button class="prev arrow"></button>',
-				nextArrow: '<button class="next arrow"></button>',
+				prevArrow: "<button class=\"prev arrow\"></button>",
+				nextArrow: "<button class=\"next arrow\"></button>",
 				slidesToScroll: 1
 			}
 		}]
 	});
-	$('.decoration_slider').slick({
+	$(".decoration_slider").slick({
 		infinite: true,
 		slidesToShow: 4,
 		slidesToScroll: 1,
@@ -48,24 +48,24 @@ $(document).ready(function () {
 			breakpoint: 1200,
 			settings: {
 				slidesToShow: 3,
-				prevArrow: '<button class="prev arrow"></button>',
-				nextArrow: '<button class="next arrow"></button>',
+				prevArrow: "<button class=\"prev arrow\"></button>",
+				nextArrow: "<button class=\"next arrow\"></button>",
 				slidesToScroll: 1
 			}
 		}, {
 			breakpoint: 992,
 			settings: {
 				slidesToShow: 2,
-				prevArrow: '<button class="prev arrow"></button>',
-				nextArrow: '<button class="next arrow"></button>',
+				prevArrow: "<button class=\"prev arrow\"></button>",
+				nextArrow: "<button class=\"next arrow\"></button>",
 				slidesToScroll: 2
 			}
 		}, {
 			breakpoint: 768,
 			settings: {
 				slidesToShow: 1,
-				prevArrow: '<button class="prev arrow"></button>',
-				nextArrow: '<button class="next arrow"></button>',
+				prevArrow: "<button class=\"prev arrow\"></button>",
+				nextArrow: "<button class=\"next arrow\"></button>",
 				slidesToScroll: 1
 			}
 		}]
